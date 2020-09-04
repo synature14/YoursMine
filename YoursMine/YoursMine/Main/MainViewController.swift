461dcc2e9591c5a06b6b05231a1a317ec517b0cf
Sutie <shuduck17@gmail.com>
2020-09-04 16:58:56 +0900

    프로젝트 초기화

---
+++ YoursMine/YoursMine/Main/MainViewController.swift
@@ -0,0 +1,20 @@
+//
+//  MainViewController.swift
+//  YoursMine
+//
+//  Created by SutieDev on 2020/09/03.
+//  Copyright © 2020 developers. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

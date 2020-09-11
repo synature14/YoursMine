489ae808f828ba882dc7f60d30883ad39c319844
hongjuyeon <coke1317@naver.com>
2020-09-11 17:56:19 +0900

    PW Setting VC UI, 파일 추가

---
+++ YoursMine/YoursMine/앱가입/PWSettingViewController.swift
@@ -0,0 +1,30 @@
+//
+//  PWSettingViewController.swift
+//  YoursMine
+//
+//  Created by hongjuyeon_dev on 2020/09/11.
+//  Copyright © 2020 developers. All rights reserved.
+//
+
+import UIKit
+
+class PWSettingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}

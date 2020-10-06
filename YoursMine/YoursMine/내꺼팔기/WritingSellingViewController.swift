d7228b918cdbdea6d236d670ec6c90d54c161d5d
hongjuyeon <coke1317@naver.com>
2020-10-06 18:18:00 +0900

    내꺼팔기 UI

--- YoursMine/YoursMine/내꺼팔기/WritingSellingViewController.swift
+++ YoursMine/YoursMine/내꺼팔기/WritingSellingViewController.swift
@@ -10,10 +10,32 @@ import UIKit
 
 class WritingSellingViewController: UIViewController {
 
+    @IBOutlet var photoImages: [UIImageView]!
+    @IBOutlet weak var txtFldTitle: UITextField!
+    @IBOutlet weak var txtFldCnt: UITextField!
+    @IBOutlet weak var txtFldPerson: UITextField!
+    @IBOutlet weak var textView: UITextView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
     }
+    
+    @IBAction func close(_ sender: UIButton) {
+        
+    }
+    
+    @IBAction func finishWriting(_ sender: UIButton) {
+        
+    }
+    
+    // 사진 고르기
+    @IBAction func selectPhoto(_ sender: UIButton) {
+        
+    }
+    
+    
+    
     
     static func create() -> WritingSellingViewController {
         let sb = UIStoryboard(name: "SellingTab", bundle: nil)

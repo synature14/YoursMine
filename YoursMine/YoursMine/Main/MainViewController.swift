b2a20445a1e0ba070538d6ad60e06e7fe662eb29
Sutie <shuduck17@gmail.com>
2020-10-07 10:52:22 +0900

    탭바 타이틀 - on/off label 추가

--- YoursMine/YoursMine/Main/MainViewController.swift
+++ YoursMine/YoursMine/Main/MainViewController.swift
@@ -17,6 +17,14 @@ class MainViewController: UIViewController {
     @IBOutlet weak var chattingContainerView: UIView!
     @IBOutlet weak var myMainContainerView: UIView!
     
+    @IBOutlet weak var buyingTabLabel: UILabel!
+    @IBOutlet weak var sellingTabLabel: UILabel!
+    @IBOutlet weak var chattingTabLabel: UILabel!
+    @IBOutlet weak var myTabLabel: UILabel!
+    
+    private let tabOnColor = #colorLiteral(red: 1, green: 0.9294117647, blue: 0.768627451, alpha: 1)
+    private let tabOffColor = UIColor.lightGray
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
@@ -51,12 +59,17 @@ class MainViewController: UIViewController {
     
     
     @IBAction func tabBarSelect(_ sender: UIButton) {
+        resetTabLabels()
+        
         switch sender.tag {
         case 0: // 니꺼사기
             sellingContainerView.isHidden = true
             chattingContainerView.isHidden = true
             myMainContainerView.isHidden = true
             buyingContainerView.isHidden = false
+            
+            buyingTabLabel.textColor = tabOnColor
+            buyingTabLabel.font = UIFont(name: "NanumBarunGothic_Bold", size: 15.0)
             
         case 1: // 내꺼팔기
             let vc = SellingViewController.create()
@@ -69,6 +82,9 @@ class MainViewController: UIViewController {
 //                sellingContainerView.isHidden = false
             })
             
+            sellingTabLabel.textColor = tabOnColor
+            sellingTabLabel.font = UIFont(name: "NanumBarunGothic_Bold", size: 15.0)
+            
         case 2: // 채팅
             buyingContainerView.isHidden = true
             sellingContainerView.isHidden = true
@@ -76,16 +92,34 @@ class MainViewController: UIViewController {
             
             chattingContainerView.isHidden = false
             
+            chattingTabLabel.textColor = tabOnColor
+            chattingTabLabel.font = UIFont(name: "NanumBarunGothic_Bold", size: 15.0)
+            
         case 3: // MY
             buyingContainerView.isHidden = true
             sellingContainerView.isHidden = true
             chattingContainerView.isHidden = true
             
             myMainContainerView.isHidden = false
+            
+            myTabLabel.textColor = tabOnColor
+            myTabLabel.font = UIFont(name: "NanumBarunGothic_Bold", size: 15.0)
         default:
             break
         }
         
+    }
+    
+    private func resetTabLabels() {
+        buyingTabLabel.textColor = tabOffColor
+        sellingTabLabel.textColor = tabOffColor
+        chattingTabLabel.textColor = tabOffColor
+        myTabLabel.textColor = tabOffColor
+        
+        buyingTabLabel.font = UIFont(name: "NanumBarunGothic_Regular", size: 15.0)
+        sellingTabLabel.font = UIFont(name: "NanumBarunGothic_Regular", size: 15.0)
+        chattingTabLabel.font = UIFont(name: "NanumBarunGothic_Regular", size: 15.0)
+        myTabLabel.font = UIFont(name: "NanumBarunGothic_Regular", size: 15.0)
     }
     
 }

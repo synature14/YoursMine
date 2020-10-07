6ea73c3272376ea6b19822fc6e477ab7f5c44745
Sutie <shuduck17@gmail.com>
2020-10-07 10:32:46 +0900

    니꺼사기 탭 배경색 white로 통일, 채팅탭 1 depth 추가

---
+++ YoursMine/YoursMine/채팅/ChattingTableViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ChattingTableViewController.swift
+//  YoursMine
+//
+//  Created by SutieDev on 2020/10/07.
+//  Copyright © 2020 developers. All rights reserved.
+//
+
+import UIKit
+
+class ChattingTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+ 
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 2
+    }
+}
